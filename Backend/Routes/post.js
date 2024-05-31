3bb2a97e63f4e9e5fdc8f59ab30d3593b816af56
TheDev05 <a.singh725098@gmail.com>
2024-05-31 15:45:14 +0530

    UPD

--- Backend/Routes/post.js
+++ Backend/Routes/post.js
@@ -9,9 +9,18 @@ router.post("/", async (req, res) => {
     const day = new Date();
     req.body.date = `${day.getDate()}-${day.getMonth()}-${day.getFullYear()}`;
 
+    const temp = {
+      title: "Sample Title",
+      message: "Sample Message",
+      date: "31-4-2024",
+      isLiked: false,
+      first_name: "Hardcoded Name",
+      email: "aa@aa.com",
+    };
+
     await database();
     const collection = post_model;
-    const response = await collection.create(req.body);
+    const response = await collection.create(temp);
     res.status(200).json({ success: true, message: "Post Created" });
   } catch (error) {
     console.log(error);
